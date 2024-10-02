d9280e9c363fa06174ec52a06bc5124fe7adaf51
Vitória Miranda <vitoriamirandaa5@gmail.com>
2024-10-02 19:47:01 -0300

    fim

--- scripts.js
+++ scripts.js
@@ -194,7 +194,22 @@ function searchNotes(search) {
 function exportData() {
     const notes = getNotes()
 
-    // separa o dado por , quebra linha
+    const csvString = [
+        ["ID", "Conteúdo", "Fixado?"],
+        ...notes.map((note) => [note.id, note.content, note.fixed])
+    ].map((e) => e.join(",")).join("\n");
+
+    const element = document.createElement("a")
+
+    element.href = "data:text/csv;charset=utf-8," + encodeURI(csvString);
+
+    element.target = "_blank"
+
+    element.download = "notes.csv";
+
+    element.click();
+
+    
 };
 
 // Eventos
